43272db4e6024add202870df57e5061e38e70742
Abdourahmane Barry <abdourahmanebarry2045@gmail.com>
2025-12-21 21:55:47 +0000

    ajoute moi une section statistique juste en bas du header pour notre imp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Clubs from '@/components/sections/clubs';
 import Events from '@/components/sections/events';
 import Hero from '@/components/sections/hero';
 import Membership from '@/components/sections/membership';
+import Stats from '@/components/sections/stats';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Stats />
       <About />
       <Clubs />
       <Events />
