ffb4c6b0128b413afbd424aaa44028e21de9de2f
Morlaye Sylla <smorlaye313@gmail.com>
2025-12-27 20:23:56 +0000

    le fichier y ait déjà

--- src/components/sections/events.tsx
+++ src/components/sections/events.tsx
@@ -5,11 +5,12 @@ import { Badge } from '@/components/ui/badge';
 import { Card, CardContent } from '@/components/ui/card';
 import ReboisementImage from '../../../image/Reboisement.jpeg';
 import LeadershipImage from '../../../image/Leadership.jpg';
+import OrphelinatsImage from '../../../image/Orphelinats.jpg';
 
 const events = [
   { title: 'Campagne de Reboisement', category: 'Environnement', date: '12 Oct 2024', img: ReboisementImage },
   { title: 'Séminaire Leadership', category: 'Éducation', date: '05 Nov 2024', img: LeadershipImage },
-  { title: 'Dons aux Orphelinats', category: 'Social', date: '20 Déc 2024', img: 'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' },
+  { title: 'Dons aux Orphelinats', category: 'Social', date: '20 Déc 2024', img: OrphelinatsImage },
 ];
 
 export default function Events() {
