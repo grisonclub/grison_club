6f3f021f47ffe6fd564a8843bc310150aa2ea066
Morlaye Sylla <smorlaye313@gmail.com>
2025-12-27 19:13:17 +0000

    sur la section nos projets recents remplace moi la photo campagne de reb

--- src/components/sections/events.tsx
+++ src/components/sections/events.tsx
@@ -3,9 +3,10 @@ import { Button } from '@/components/ui/button';
 import { ArrowRight, Calendar, ChevronRight } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent } from '@/components/ui/card';
+import ReboisementImage from '../../../image/Reboisement.jpeg';
 
 const events = [
-  { title: 'Campagne de Reboisement', category: 'Environnement', date: '12 Oct 2024', img: 'https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' },
+  { title: 'Campagne de Reboisement', category: 'Environnement', date: '12 Oct 2024', img: ReboisementImage },
   { title: 'Séminaire Leadership', category: 'Éducation', date: '05 Nov 2024', img: 'https://images.unsplash.com/photo-1544531679-6064e2a67c0f?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' },
   { title: 'Dons aux Orphelinats', category: 'Social', date: '20 Déc 2024', img: 'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' },
 ];
