4d7a97874f82bb44d4a4de2d609fa2e055067785
Morlaye Sylla <smorlaye313@gmail.com>
2025-12-31 20:25:48 +0000

    Avant le défilement je voudrais que les éléments du header  soit visible

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -90,7 +90,7 @@ export default function Header() {
                 'px-4 py-2 rounded-md font-medium transition-colors hover:text-primary',
                 isScrolled
                   ? 'text-slate-600 dark:text-slate-300'
-                  : 'text-slate-200'
+                  : 'text-white hover:text-primary'
               )}
             >
               {link.label}
